b93b3f977b19cdc3ceffaececae4c8d297cdf437
vyin <limcolin0809@gmail.com>
2025-05-13 09:29:48 -0400

    Marcus - added eat function, added label for score when the elephant eats

--- Elephant.java
+++ Elephant.java
@@ -23,4 +23,14 @@ public class Elephant extends Actor
         
         removeTouching(Apple.class);
     }
+    
+    public void eat()
+    {
+        removeTouching(Apple.class);
+        MyWorld world = (MyWorld) getWorld();
+        world.createApple();
+    }
 }
+
+
+    
--- MyWorld.java
+++ MyWorld.java
@@ -7,7 +7,19 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        Label scoreLabel = new Label(0, 20);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+
+    
+    
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
