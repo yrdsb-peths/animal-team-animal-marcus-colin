77e707079f8a4c491f135c985312b5ebbb1c1f2e
vyin <limcolin0809@gmail.com>
2025-05-13 09:16:02 -0400

    Marcus - added elephant with keyboard movement, added apple and elephant ability to removes apple

--- Elephant.java
+++ Elephant.java
@@ -8,12 +8,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephant-trumpets-growls-6047.mp3");
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+        
+        removeTouching(Apple.class);
     }
 }
